4eec783e5a94e6df577948b7af28ace16c82abf3
YukiAkubi <kazuyuki19991207@gmail.com>
2023-12-26 20:27:13 +0900

    ダウンロードページ用のWebプロジェクトを用意

---
+++ DownloadPage/DownloadPage/DownloadPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DownloadPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
